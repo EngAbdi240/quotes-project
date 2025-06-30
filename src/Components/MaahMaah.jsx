f91c7d2313a6b1afac2e2b39f34c745536b386fc
abdi <engabdi85@gmail.com>
2025-06-30 11:14:19 -0700

    responsive apdate

--- src/Components/MaahMaah.jsx
+++ src/Components/MaahMaah.jsx
@@ -44,7 +44,7 @@ function MaahMaah (){
 
         <h1 style={{display: index === quotes.length-1 ? "block" : "none"}} className="text-3xl text-center text-red-700">Dhamaad</h1>
         <h1 style={{color: index === 4 ? "red" : ""}} className="sm:text-3xl text-center text-xl  font-semibold">{quotes[index]}</h1>
-        <button onClick={handleDecrement} className="bg-blue-600 px-14 py-3 rounded-lg text-white sm:ml-[40%] mt-20 ml-5">previous</button>
+        <button onClick={handleDecrement} className="bg-blue-600 px-14 py-3 rounded-lg text-white sm:ml-[40%] mt-20 ">previous</button>
         <button onClick={handleIncrement} className="bg-blue-600 px-14 py-3 rounded-lg text-white ml-10">Next</button>
         
     </div>
